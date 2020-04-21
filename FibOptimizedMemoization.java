04fdf6ffa41a85a5f2a3b85d52e66082419cbf34
adarshshankar <sahaiadarsh@gmail.com>
2020-04-21 19:32:16 +0530

    Recursive Fibonacci using Memoization
    Recursive Fibonacci Implementation using Memoization

---
+++ FibOptimizedMemoization.java
@@ -0,0 +1,32 @@
+package com.adarsh.Test1;
+
+public class FibOptimizedMemoization {
+    public static long[] fibArray = new long[250];
+
+    public static void main(String[] args) {
+        fibArray[0] = 1;
+        fibArray[1] = 1;
+
+        long preTime = System.currentTimeMillis();
+        System.out.println(fib(200));
+        long postTime = System.currentTimeMillis();
+        System.out.println("total time to compute=== " + (postTime - preTime));
+
+    }
+
+    public static long fib(long n) {
+
+        if (n <= 1)
+            return n;
+        else if (fibArray[(int) n] != 0)
+            return fibArray[(int) n];
+        else {
+
+            long fibValue = fib(n - 1) + fib(n - 2);
+            fibArray[(int) n] = fibValue;
+            return fibValue;
+        }
+
+
+    }
+}
